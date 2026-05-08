cb55d8995c1e79b542b8214024ab943cc7223ddb
Supriya <supriyagosavi14@gmail.com>
2026-05-08 23:55:57 +0200

    first commit

---
+++ sample_file.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+# ---------------------------------------------------
+# PAGE TITLE
+# ---------------------------------------------------
+st.title("Supermarket Sales Dashboard")
+st.write("This dashboard allow us to explore Supermarket sales data using interactive streamlit widgets")
+
+st.divider()
