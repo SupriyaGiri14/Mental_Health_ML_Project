c36354863df9b9e818d1bb4663519cdfb4b651cb
Supriya <supriyagosavi14@gmail.com>
2026-05-15 07:59:57 +0200

    new code

--- sample_file.py
+++ sample_file.py
@@ -178,8 +178,14 @@ if st.button("Predict"):
         with st.spinner("Processing..."):
             time.sleep(1)
         if prediction[0] == 1:
-            message = f"Prediction: {name }, \nSeeking treatment is likely recommended."
+            message = f"Prediction: {name }, Seeking treatment is likely recommended."
             st.error(message)
+            st.markdown(f"""
+                ### Prediction Result
+                **Name:** {name}
+
+                Seeking treatment is likely recommended.
+                """)
         else:
             message = f"Prediction: {name} , Seeking treatment is likely not required."
             st.success(message)
